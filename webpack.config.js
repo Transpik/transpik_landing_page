2ad6d3776d624503235ce8f00ae2cde76b329cf6
R4JxF60 <rajithakumaraprog@gmail.com>
2022-07-16 13:08:46 +0530

    fix: dev-server GET 404 issue

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
         open: true,
         hot: true,
         historyApiFallback: {
-            index: "document.html",
+            index: "index.html",
         },
     },
 } 
