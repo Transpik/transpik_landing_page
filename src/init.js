c33585b1efc1f25fa2d2a4216e1daa08d5041570
R4JxF60 <rajithakumaraprog@gmail.com>
2022-07-16 12:49:36 +0530

    update: include react and main style

---
+++ src/init.js
@@ -0,0 +1,4 @@
+import * as ReactDOM from 'react-dom/client';
+import "./styles/main.css";
+
+ReactDOM.createRoot(document.getElementById('root')).render(<h1>Welcome to Transpik landing page!</h1>);
