48571cc041b10b2b6f585a6dce1429756c39d900
R4JxF60 <rajithakumaraprog@gmail.com>
2022-07-16 12:59:40 +0530

    fix: include react

--- src/init.js
+++ src/init.js
@@ -1,4 +1,5 @@
-import * as ReactDOM from 'react-dom/client';
+import * as ReactDOM from "react-dom/client";
+import React from "react";
 import "./styles/main.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<h1>Welcome to Transpik landing page!</h1>);
+ReactDOM.createRoot(document.getElementById("root")).render(<h1>Welcome to Transpik!</h1>);
