c3f31e563f10e3d0d9c92311202e8f0779014c88
Imal-Kovinda <83854345+Imal-Kovinda@users.noreply.github.com>
2022-08-21 21:37:28 +0530

    dashboard

--- src/pages/Tracking.js
+++ src/pages/Tracking.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import { Routes, Route, useNavigate, Navigate } from 'react-router-dom';
-import Map from './Map';
+// import Map from './Map';
 
 class Tracking extends React.Component {
   constructor(props) {
@@ -10,18 +10,18 @@ class Tracking extends React.Component {
   render() {
     const trackMe = () => {
       console.log("HI");
-      <Map />
+      // <Map />
       // navigate('/Map');
 
     }
     // const navigate = useNavigate();
 
     // const navigateToMap = () => {
-    //   navigate('/Map')
+    //   navigate('/Map');
     // }
 
     return (
-      <div className="flex flex-col items-center justify-center container align-middle w-full h-screen bg-secondary-light_orange">
+      <div className="flex flex-col items-center justify-center container mx-auto align-middle w-full h-screen bg-secondary-light_orange">
         <div className="flex items-center justify-center w-full h-screen">
           <div class="flex flex-col items-center shadow-xl w-[600px] h-[300px] bg-white rounded-md">
             <div className="flex items-center justify-center topic w-full h-[70px] font-bold">
@@ -33,14 +33,20 @@ class Tracking extends React.Component {
             <div className="input_btn flex flex-row items-center justify-center border-2 w-[500px] h-[150px] ">
               <input className="input-area text-sm w-[220px] h-[50px] bg-red-50 rounded-md ml-9 border-slate-300 py-2 pl-3 hover:bg-orange-100 placeholder:italic outline-orange" placeholder="Order ID" />
 
-              <button className="track bg-orange hover:bg-red-500 w-[100px] h-[50px] rounded ml-4 text-white font-bold" onClick={() => trackMe()}>Track</button>
+              <button className="track bg-orange hover:bg-red-500 w-[100px] h-[50px] rounded ml-4 text-white font-bold" onClick={() => trackMe}>Track</button>
 
             </div>
           </div>
+          {/* <Routes>
+            <Route path="/Map" element={<Map />}>
+
+            </Route>
+          </Routes> */}
         </div>
       </div>
     );
   }
+
 }
 
 export default Tracking;
